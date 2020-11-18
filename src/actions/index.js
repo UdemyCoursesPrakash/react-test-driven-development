7341e3a22c182a68e019c89a33b97a911177a527
Prakash Nale -X (prnale - ZENSAR TECHNOLOGIES INC at Cisco) <prnale@partnet.cisco.com>
2020-11-18 15:25:24 +0530

    added moxios

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,10 @@
+import axios from 'axios';
 import getLetterMatchCount from "../helper/getLetterMatchCount"
 
 export const actionTypes = {
     CORRECT_GUESS : 'CORRECT_GUESS',
-    GUESSED_WORD  : 'GUESSED_WORD'
+    GUESSED_WORD  : 'GUESSED_WORD',
+    SET_SECRET_WORD : 'SET_SECRET_WORD'
 } 
 
 export const correctGuess = () =>{
@@ -35,4 +37,17 @@ export const guessWord = (guessedWord) => (dispacth , getState) =>{
             type : actionTypes.CORRECT_GUESS
         })
     }
+}
+
+export const getSecretWord = () => (dispatch , getState) =>{
+    return axios.get('../../public/api.json')
+   .then((res)=>{
+       console.log(res.data);
+     dispatch({
+         type : actionTypes.SET_SECRET_WORD,
+         payload : res.data
+     })
+   }).catch((err)=>{
+     console.log(err);
+   })
 }
--- src/actions/index.test.js
+++ src/actions/index.test.js
@@ -1,4 +1,7 @@
-import { actionTypes , correctGuess , asyncCorrectGuess } from './index';
+import moxios from 'moxios';
+import { storeFactory } from '../storeFactory/storeFactory';
+
+import { actionTypes , correctGuess , asyncCorrectGuess, getSecretWord } from './index';
 
 describe('test action creators using imported action from action creators file',()=>{
     it('correct guess action should return "type:CORRECT_GUESS"',()=>{
@@ -17,6 +20,34 @@ describe('test action creators using mock function',()=>{
         expect(dispatchMock.mock.calls[0][0]).toEqual({
             type : actionTypes.CORRECT_GUESS,
             payload : false
+        })
+    })
+})
+
+
+describe('getSecretWord action creator test',()=>{
+    beforeEach(()=>{
+        moxios.install(); // if we have axios instance imported we can pass that instance to install function
+    })
+    afterEach(()=>{
+        moxios.uninstall();
+    })
+
+    test('adds response to the state',()=>{
+        const secretWord = 'party';
+        const store = storeFactory();
+
+        moxios.wait(()=>{
+            const request = moxios.requests.mostRecent();
+            request.respondWith({
+                status : 200,
+                response : secretWord
+            })
+        })
+
+        return store.dispatch(getSecretWord()).then(()=>{
+            const newState = store.getState();
+            expect(newState.secretWord).toBe(secretWord);
         })
     })
 })
